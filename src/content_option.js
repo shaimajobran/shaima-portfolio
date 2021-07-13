ff16294ef713f6c0f74eed38a02476573e4d073c
Shaima Jobran <shaimajobran22@gmail.com>
2021-07-13 18:25:56 -0400

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,113 @@
+const logotext = "SJ";
+const meta = {
+    title: "SHAIMA JOBRAN",
+    description: "I’m Shaima Jobran Full stack devloper",
+};
+
+const introdata = {
+    title: "I’m Shaima Jobran",
+    animated: {
+        first: "Full Stack Developer",
+        second: "I Build React Apps ",
+        third: "I Love Coding !",
+    },
+    description: "Full Stack Developer",
+    your_img_url:"https://github.com/shaimajobran/React-Portfoilo/blob/main/src/assets/images/5.JPG?raw=true"
+};
+
+const dataabout = {
+    title: " Get to know Me",
+    aboutme: "Hi! I am Shaima! A full stack developer  using new technologies to build web applications. I enjoy Back End as well as Front End developing . I am passionate about web development and programming because I love to make apps that help people and communities. Recently completed a Full Stack developer bootcamp. Excellent ability to learn new technologies and tools. Eager to build my professional experience and apply recent knowledge.",
+};
+
+
+
+const skills = [
+    {
+        name: "HTML & CSS",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 70,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const Langauges = [{
+        title: "Languages I use ",
+        description: "HTML,CSS,JS,Express,Node.js,JQuery,SQL,Mongo,React.",
+    }
+];
+
+const dataportfolio = [{
+        img: "https://github.com/shaimajobran/MVC-Tech-Blog/blob/main/public/images/1.jpg?raw=true",
+        desctiption: "MVC",
+        link: "https://quiet-anchorage-14204.herokuapp.com/",
+    },
+    {
+        img: "https://github.com/shaimajobran/Budget-Tracker/blob/main/public/images/3.jpg?raw=true",
+        desctiption: "Budget-Tracker",
+        link: "https://serene-grand-teton-68663.herokuapp.com/",
+    },
+    {
+        img: "https://github.com/shaimajobran/Fitness-Tracker/blob/main/public/images/2.jpg?raw=true",
+        desctiption: "Fitness-Tracker",
+        link: "https://guarded-hollows-80756.herokuapp.com/?id=60dbcfb50b3108001511d046",
+    },
+    
+    {
+        img: "https://github.com/shaimajobran/Note-Taker/blob/main/images/Screenshot-2021-05-07-212809.jpg?raw=true",
+        desctiption: "Note-Taker",
+        link: "https://ancient-waters-55405.herokuapp.com/",
+    },
+    {
+        img: "https://github.com/shaimajobran/Musical/blob/main/assets/images/Screenshot-2021-04-08-205028.jpg?raw=true",
+        desctiption: "Musical",
+        link: "https://crsmith01.github.io/Musical/",
+    },
+    {
+        img: "https://github.com/shaimajobran/lightCville/blob/master/public/images/3.jpg?raw=true",
+        desctiption: "LightCville",
+        link: "https://lightcville.herokuapp.com/login",
+    },
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "shaimajobran22@gmail.com",
+    YOUR_FONE: "(410)428-1438",
+    description: "",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/shaimajobran",
+    linkedin: "https://www.linkedin.com/in/shaima-jobran-323472172/",
+    twitter: "https://twitter.com/shaimajobran",
+    // resume:"file:///C:/Users/shaim/Downloads/Resume_2.docx.pdf",
+};
+const footer ={
+    
+}
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    skills,
+    Langauges,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+    footer,
+};
